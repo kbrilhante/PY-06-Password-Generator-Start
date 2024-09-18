6f5ba9e0ad1e9ee5fb5f3edb0d49d63c4ea09ffa
Karina <kbrilhante@gmail.com>
2024-09-18 18:15:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import random
 
 # Password Generator
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+upper = list('abcdefghijklmnopqrstuvwxyz')
+lower = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+numbers = list('0123456789')
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("-x-x-x-x- PASSWORD GENERATOR -x-x-x-x-\n")
-num_letters = int(input("How many letters would you like in your password? ")) 
+num_upper = int(input("How many upper caseletters would you like in your password? "))
+num_lower = int(input("How many lower case letters would you like in your password? "))
 num_symbols = int(input("How many symbols would you like? "))
 num_numbers = int(input("How many numbers would you like? "))
 
 # DO NOT ERASE ROWS ABOVE!
-# Make a program that generates a strong password
-# that randomly picks the chosen amount of letters,
-# symbols and numbers
+# TODO: Make a program that generates a strong password that randomly picks the chosen amount of letters, symbols and numbers
