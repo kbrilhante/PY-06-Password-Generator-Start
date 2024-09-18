cf4037d1f154ba0c161cc6d7552ebb6f34afdf62
Karina <58565453+kbrilhante@users.noreply.github.com>
2024-09-18 18:27:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ numbers = list('0123456789')
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("-x-x-x-x- PASSWORD GENERATOR -x-x-x-x-\n")
-num_upper = int(input("How many upper caseletters would you like in your password? "))
+num_upper = int(input("How many upper case letters would you like in your password? "))
 num_lower = int(input("How many lower case letters would you like in your password? "))
 num_symbols = int(input("How many symbols would you like? "))
 num_numbers = int(input("How many numbers would you like? "))
