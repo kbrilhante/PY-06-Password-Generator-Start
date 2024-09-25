6a91f9dc9a69283c878158691c362afbb8a0798e
Karina <58565453+kbrilhante@users.noreply.github.com>
2024-09-25 15:31:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import random
 
 # Password Generator
 
-upper = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-lower = list('abcdefghijklmnopqrstuvwxyz')
-numbers = list('0123456789')
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+upper = tuple('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+lower = tuple('abcdefghijklmnopqrstuvwxyz')
+numbers = tuple('0123456789')
+symbols = ('!', '#', '$', '%', '&', '(', ')', '*', '+')
 
 print("-x-x-x-x- PASSWORD GENERATOR -x-x-x-x-\n")
 num_upper = int(input("How many upper case letters would you like in your password? "))
